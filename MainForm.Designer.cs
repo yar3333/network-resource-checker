294277c02737963d0b07744efd1adea104568925
Yaroslav Sivakov <yar3333@gmail.com>
2017-05-17 14:56:22 +0300

    + init

---
+++ MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NetworkResourceChecker
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.tbHost = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.btStart = new System.Windows.Forms.Button();
+			this.btStop = new System.Windows.Forms.Button();
+			this.cbPort = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.Text = "Network resource checker";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+			// 
+			// tbHost
+			// 
+			this.tbHost.Location = new System.Drawing.Point(47, 15);
+			this.tbHost.Name = "tbHost";
+			this.tbHost.Size = new System.Drawing.Size(367, 20);
+			this.tbHost.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(29, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Host";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(426, 18);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(26, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Port";
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 30000;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// btStart
+			// 
+			this.btStart.Location = new System.Drawing.Point(134, 55);
+			this.btStart.Name = "btStart";
+			this.btStart.Size = new System.Drawing.Size(131, 23);
+			this.btStart.TabIndex = 4;
+			this.btStart.Text = "Start";
+			this.btStart.UseVisualStyleBackColor = true;
+			this.btStart.Click += new System.EventHandler(this.btStart_Click);
+			// 
+			// btStop
+			// 
+			this.btStop.Enabled = false;
+			this.btStop.Location = new System.Drawing.Point(303, 55);
+			this.btStop.Name = "btStop";
+			this.btStop.Size = new System.Drawing.Size(131, 23);
+			this.btStop.TabIndex = 5;
+			this.btStop.Text = "Stop";
+			this.btStop.UseVisualStyleBackColor = true;
+			this.btStop.Click += new System.EventHandler(this.btStop_Click);
+			// 
+			// cbPort
+			// 
+			this.cbPort.FormattingEnabled = true;
+			this.cbPort.Location = new System.Drawing.Point(458, 15);
+			this.cbPort.Name = "cbPort";
+			this.cbPort.Size = new System.Drawing.Size(101, 21);
+			this.cbPort.TabIndex = 6;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(571, 97);
+			this.Controls.Add(this.cbPort);
+			this.Controls.Add(this.btStop);
+			this.Controls.Add(this.btStart);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tbHost);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Network resource checker";
+			this.Resize += new System.EventHandler(this.MainForm_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.TextBox tbHost;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.Button btStart;
+		private System.Windows.Forms.Button btStop;
+		private System.Windows.Forms.ComboBox cbPort;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkResourceChecker
+{
+	public partial class MainForm : Form
+	{
+		const int CHECK_PERIOD_SECONDS = 60*60;
+
+		readonly KnownPort[] knownPorts =
+		{
+			new KnownPort("HTTP / 80", 80), 
+            new KnownPort("HTTPS / 81", 81), 
+            new KnownPort("FTP / 21", 21),
+            new KnownPort("RDP / 3389", 3389)
+		};
+
+		bool running;
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			timer.Interval = CHECK_PERIOD_SECONDS * 1000;
+
+			cbPort.DataSource = knownPorts;
+			cbPort.DisplayMember = "name";
+			cbPort.ValueMember = "port";
+
+			if (!string.IsNullOrEmpty(getProp<string>("host"))) tbHost.Text = getProp<string>("host");
+			cbPort.Text = getProp<int>("port").ToString();
+		}
+
+		private void MainForm_Resize(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized)
+			{
+				notifyIcon.Icon = SystemIcons.Application;
+				notifyIcon.Visible = true;
+				Hide();
+			}
+			else if (WindowState == FormWindowState.Normal)
+			{
+				notifyIcon.Visible = false;
+			}
+		}
+
+		async void timer_Tick(object sender, EventArgs e)
+		{
+			if (!running) return;
+
+			if (tbHost.Text != "" &&  getSelectedPort() != 0)
+			{
+				if (await isResourceAvailable(tbHost.Text, getSelectedPort()))
+				{
+					notifyIcon.Icon = SystemIcons.Application;
+					notifyIcon.ShowBalloonTip(30*1000, "Resource avalilable", tbHost.Text + " : " + getSelectedPort(), ToolTipIcon.Info);
+					btStop_Click(null, null);
+				}
+			}
+		}
+
+		private void notifyIcon_Click(object sender, EventArgs e)
+		{
+			notifyIcon.Visible = false;
+			Show();
+			WindowState = FormWindowState.Normal;
+		}
+
+		async Task<bool> isResourceAvailable(string host, int port)
+		{
+			using (var tcpClient = new TcpClient())
+			{
+				try
+				{
+					await tcpClient.ConnectAsync(host, port);
+					return true;
+				}
+				catch
+				{
+					return false;
+				}
+			}
+		}
+
+		private void btStart_Click(object sender, EventArgs e)
+		{
+			if (tbHost.Text != "") setProp("host", tbHost.Text);
+			if (cbPort.Text != "") setProp("port", getSelectedPort());
+			Properties.Settings.Default.Save();
+
+			running = true;
+			btStart.Enabled = false;
+			btStop.Enabled = true;
+			tbHost.ReadOnly = true;
+			cbPort.Enabled = false;
+		}
+
+		private void btStop_Click(object sender, EventArgs e)
+		{
+			running = false;
+			btStart.Enabled = true;
+			btStop.Enabled = false;
+			tbHost.ReadOnly = false;
+			cbPort.Enabled = true;
+		}
+
+		T getProp<T>(string name)
+		{
+			return (T)Properties.Settings.Default[name];
+		}
+
+		void setProp<T>(string name, T v)
+		{
+			Properties.Settings.Default[name] = v;
+		}
+
+		int getSelectedPort()
+		{
+			try
+			{
+				return (int?)cbPort.SelectedValue ?? int.Parse(cbPort.Text);
+			}
+			catch
+			{
+				return 0;
+			}
+		}
+	}
+}
